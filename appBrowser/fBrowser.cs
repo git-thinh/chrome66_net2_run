1534ee3375a85b781f18d9f8b98c1bc7fd02f023
Mr Thinh <33323583+git-thinh@users.noreply.github.com>
2018-09-17 23:09:45 +0700

    a

--- appBrowser/fBrowser.cs
+++ appBrowser/fBrowser.cs
@@ -2,11 +2,27 @@
 using System.Collections.Generic;
 using System.Text;
 using System.Windows.Forms;
+using Xilium.CefGlue.WindowsForms;
 
 namespace appBrowser
 {
     public class fBrowser: Form
     {
-
+        private CefWebBrowser browser;
+        public fBrowser()
+        {
+            browser = new CefWebBrowser
+            {
+                Dock = DockStyle.Fill
+            };
+            browser.Parent = this;
+            browser.BrowserCreated += (se, ev) => {
+                browser.Browser.GetMainFrame().LoadUrl("https://google.com.vn");
+                //browser.Browser.GetMainFrame().LoadUrl("http://localhost:60000/index.html");
+                //browser.Browser.Reload();
+                //browser.Browser.GetMainFrame().LoadUrl("about:blank");
+                //browser.Browser.GetMainFrame().LoadUrl("http://localhost:60000/index.html"); 
+            };
+        }
     }
 }
