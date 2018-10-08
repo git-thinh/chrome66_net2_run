a4b705d945012f9d94158984b9d16ad01c28d2c5
Mr Thinh <33323583+git-thinh@users.noreply.github.com>
2018-10-08 15:12:18 +0700

    q

--- appBrowser/fBrowser.cs
+++ appBrowser/fBrowser.cs
@@ -1,51 +1,92 @@
 ﻿using System;
 using System.Collections.Generic;
+using System.Drawing;
+using System.IO;
+using System.Runtime.InteropServices;
 using System.Text;
 using System.Windows.Forms;
+using Xilium.CefGlue;
 using Xilium.CefGlue.WindowsForms;
 
 namespace appBrowser
 {
-    public class fBrowser: Form
+    public class fBrowser : Form
     {
-        private CefWebBrowser browser;
-        //const string url = "https://google.com.vn";
-        const string url = "https://vnexpress.net/";
-        //const string url = "http://localhost";
+        private CefWebBrowser _BROWSER;
+        private string _URL = string.Empty;
 
         public fBrowser()
         {
-
-            browser = new CefWebBrowser
+            _BROWSER = new CefWebBrowser
             {
                 Dock = DockStyle.Fill
             };
-            browser.Parent = this;
-            browser.BrowserCreated += (se, ev) => {
-                browser.Browser.GetMainFrame().LoadUrl(url);
-                //browser.Browser.GetMainFrame().LoadUrl("http://localhost:60000/index.html");
-                //browser.Browser.Reload();
-                //browser.Browser.GetMainFrame().LoadUrl("about:blank");
-                //browser.Browser.GetMainFrame().LoadUrl("http://localhost:60000/index.html"); 
+            _BROWSER.Parent = this;
+            _BROWSER.BrowserCreated += (se, ev) =>
+            {
+                if (File.Exists("url.txt")) _URL = File.ReadAllText("url.txt");
+                else _URL = "https://wwww.google.com.vn";
+                _BROWSER.Browser.GetMainFrame().LoadUrl(_URL);                
+                f_test_ShowDevTools();
             };
-            
-            TextBox txt = new TextBox() {
-                Text = url,
-                Dock = DockStyle.Bottom,
-                BorderStyle = BorderStyle.None,
-                BackColor = System.Drawing.Color.LightGray,
-                ForeColor = System.Drawing.Color.Gray
-            };
-            txt.KeyUp += (se, ev) => {
-                if(ev.KeyCode == Keys.Enter)
-                    browser.Browser.GetMainFrame().LoadUrl(txt.Text.Trim());
-            };
-            this.Controls.Add(txt);
 
-            this.Shown += (se, ev) => {
-                this.Width = 1024;
-                this.Height = 600;
+            Label lblMove = new Label()
+            {
+                Top = 0,
+                Height = 9,
+                Width = 9,
+                BackColor = Color.Orange,
+            };
+            this.Controls.Add(lblMove);
+            lblMove.MouseMove += f_form_move_MouseDown;
+
+            this.Shown += (se, ev) =>
+            {
+                lblMove.BringToFront();
+
+                this.FormBorderStyle = FormBorderStyle.None;
+                this.Top = 0;
+                this.Left = 0;
+                this.Width = 800;
+                this.Height = 480;
             };
         }
+
+        void f_test_ShowDevTools()
+        {
+            var host = _BROWSER.Browser.GetHost();
+            var wi = CefWindowInfo.Create();
+            wi.SetAsPopup(IntPtr.Zero, "DevTools");
+            host.ShowDevTools(wi, new DevToolsWebClient(), new CefBrowserSettings(), new CefPoint(0, 0));
+        }
+
+        #region [ FORM MOVE, RESIZE ]
+
+        enum MOUSE_XY { OUT, INT };
+        
+        public const int WM_NCLBUTTONDOWN = 0xA1;
+        public const int HT_CAPTION = 0x2;
+
+        [DllImportAttribute("user32.dll")]
+        public static extern int SendMessage(IntPtr hWnd, int Msg, int wParam, int lParam);
+        [DllImportAttribute("user32.dll")]
+        public static extern bool ReleaseCapture();
+
+        private void f_form_move_MouseDown(object sender, System.Windows.Forms.MouseEventArgs e)
+        {
+            if (e.Button == MouseButtons.Left)
+            {
+                ReleaseCapture();
+                SendMessage(Handle, WM_NCLBUTTONDOWN, HT_CAPTION, 0);
+            }
+        }
+
+        #endregion
+
     }
+
+    class DevToolsWebClient : CefClient
+    {
+    }
+
 }
